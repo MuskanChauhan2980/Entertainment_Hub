c9e67a0901dc4ed4c64a7c430ff2a06f7defb142
Muskan Chauhan <muskan.chauhan@redknot.casa>
2025-10-10 12:19:48 +0530

    update teh app.jsx fiel with currect producation router

--- entertainhub-frontend/src/App.jsx
+++ entertainhub-frontend/src/App.jsx
@@ -1,10 +1,10 @@
- import React from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Restaurants from "./pages/Restaurants";
 import Blog from "./pages/Blog";
-import Home from "./pages/Home"; 
-import Articles from "./pages/Articles"; 
-import Event from "./pages/Events"
+import Home from "./pages/Home";
+import Articles from "./pages/Articles";
+import Event from "./pages/Events";
 import Signup from "./pages/signup";
 import Login from "./pages/login";
 import EventDetails from "./pages/EventDetails";
@@ -21,20 +21,21 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Signup/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/verify-otp" element={<VerifyOTP/>}/>
-        <Route path="/home" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/verify-otp" element={<VerifyOTP />} />
+        <Route path="/" element={<Home />} />
         <Route path="/restaurants" element={<Restaurants />} />
-        <Route path="/blog" element={<Blog/>}/> 
-        <Route path="/article" element={<Articles/>}/> 
-        <Route path ="/event" element={<Event/>}/>
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/article" element={<Articles />} />
+        <Route path="/event" element={<Event />} />
         <Route path="/events/:id" element={<EventDetails />} />
         <Route path="/articles/:id" element={<ArticleDetails />} />
-<Route path="/models" element={<ModelsPage />} />
-<Route path="/models/:id" element={<ModelDetails />} />
-  <Route path="/calendar" element={<CalendarPage />} />
-<Route path="/premium" element={<PremiumPage />} />
+        <Route path="/models" element={<ModelsPage />} />
+        <Route path="/models/:id" element={<ModelDetails />} />
+        <Route path="/calendar" element={<CalendarPage />} />
+        <Route path="/premiumDetails" element={<PremiumPage />} />
+        <Route path="/premium" element={<SecurePayment />} />
       </Routes>
     </Router>
   );
